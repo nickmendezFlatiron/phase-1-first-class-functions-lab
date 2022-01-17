2ba1813453432b43e33ad75eee87f06a08574d06
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-01-17 00:39:58 -0500

    commit tree a5ba39a96a34ebaef783f1547a092c07b9f9ab85

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (array) {
+    return [array[0] , array[1]];
+}
+
+const returnLastTwoDrivers = function (array) {
+    const copyArray = [...array];
+    return copyArray.splice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers] ;
+
+function createFareMultiplier(number) {
+    return fare => fare * number ;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (array , aFunction){
+    if (aFunction === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(array);
+    } else if (aFunction === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(array);
+    }
+}
